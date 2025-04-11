65552dc8bb87c0bbb4f3ca8d2c006cd4a9ca1469
Jon <user@Users-MacBook-Pro.local>
2025-04-11 18:55:51 +0800

    working it out

--- src/sutd-carousel.js
+++ src/sutd-carousel.js
@@ -141,7 +141,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     let mixer = null
                     // Load model
                     const loader = new GLTFLoader();
-                    loader.load('/models/engine_model_5.1_fixed.gltf', (gltf) => {
+                    loader.load('public/models/engine model_5.1.gltf', (gltf) => {
                         const model = gltf.scene;
                         const animations = gltf.animations;
                         model.castShadow = true;    // Object that casts the shadow
